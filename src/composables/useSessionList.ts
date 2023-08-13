b4e110dbb7c3b7fbd554b55520888dd52050e882
wobsoriano <sorianorobertc@gmail.com>
2023-08-13 15:12:53 -0700

    feat: add useSessionList composable

---
+++ src/composables/useSessionList.ts
@@ -0,0 +1,24 @@
+import type { SessionResource, SetActive, SetSession } from '@clerk/types'
+import { computed } from 'vue'
+
+import { toRefs } from '@vueuse/core'
+import { useClerkProvide } from '../plugin'
+
+type UseSessionListReturn =
+  | { isLoaded: false; sessions: undefined; setSession: undefined; setActive: undefined }
+  | { isLoaded: true; sessions: SessionResource[]; setSession: SetSession; setActive: SetActive }
+
+export function useSessionList() {
+  const { clerk, isClerkLoaded } = useClerkProvide()
+  return toRefs(computed<UseSessionListReturn>(() => {
+    if (!isClerkLoaded.value || !clerk.client)
+      return { isLoaded: false, sessions: undefined, setSession: undefined, setActive: undefined }
+
+    return {
+      isLoaded: true,
+      sessions: clerk.client.sessions,
+      setSession: clerk.setSession,
+      setActive: clerk.setActive,
+    }
+  }))
+}
