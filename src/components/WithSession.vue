627d2880499b7c72ddf6cb05393ef82a3e5c7ea1
wobsoriano <sorianorobertc@gmail.com>
2023-08-14 10:19:06 -0700

    feat: add WithSession HOC

---
+++ src/components/WithSession.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { toReactive } from '@vueuse/core'
+import { useSession } from '../composables/useSession'
+
+// toReactive removes .value when accessing in template
+const props = toReactive(useSession())
+</script>
+
+<template>
+  <slot v-bind="props" />
+</template>
