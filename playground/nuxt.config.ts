793182ec719b51eb82be7747695ac05947622a8b
wobsoriano <sorianorobertc@gmail.com>
2023-10-19 10:23:24 -0700

    bump local deps

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   build: {
-    transpile: ['vue-clerk'],
+    // transpile: ['vue-clerk'],
   },
   runtimeConfig: {
     public: {
