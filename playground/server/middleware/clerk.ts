6f8caeca07e4aa1fcfe827ae2ecae351be987e57
wobsoriano <sorianorobertc@gmail.com>
2023-10-22 08:30:30 -0700

    bump local deps

--- playground/server/middleware/clerk.ts
+++ playground/server/middleware/clerk.ts
@@ -1,6 +1,3 @@
 import { withClerkMiddleware } from 'h3-clerk'
 
-export default withClerkMiddleware({
-  publishableKey: useRuntimeConfig().public.clerkPublishableKey,
-  secretKey: useRuntimeConfig().clerkSecretKey,
-})
+export default withClerkMiddleware()
