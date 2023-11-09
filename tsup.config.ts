75b73b54bf3e55120e81553d930062a821897700
wobsoriano <sorianorobertc@gmail.com>
2023-11-09 10:16:53 -0800

    add splitting

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ import vuePlugin from 'unplugin-vue/esbuild'
 export default defineConfig({
   entry: ['src/index.ts', 'src/plugin.ts', 'src/headless.ts'],
   format: ['cjs', 'esm'],
-  splitting: false,
+  splitting: true,
   clean: false,
   dts: false,
   // @ts-expect-error: Internal
