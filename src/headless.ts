7845379a89af717bc8b38c996d916eb20963bd9f
wobsoriano <sorianorobertc@gmail.com>
2024-04-09 11:39:25 -0700

    chore: export plugin type

--- src/headless.ts
+++ src/headless.ts
@@ -3,6 +3,8 @@ import type { Plugin } from 'vue'
 import type { VueClerkOptions } from './createClerkInstance'
 import { createClerkInstance } from './createClerkInstance'
 
+export type { VueClerkOptions }
+
 export const clerkPlugin: Plugin = {
   install(app, options: VueClerkOptions) {
     const { publishableKey, domain, ...loadOptions } = options
