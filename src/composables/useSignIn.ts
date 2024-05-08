dd8f1121d941676ac5e25389594cd1b39e36b7ba
wobsoriano <sorianorobertc@gmail.com>
2024-05-07 17:58:58 -0700

    chore!: remove setSession function from useSignIn

--- src/composables/useSignIn.ts
+++ src/composables/useSignIn.ts
@@ -1,23 +1,22 @@
-import type { SetActive, SetSession, SignInResource } from '@clerk/types'
+import type { SetActive, SignInResource } from '@clerk/types'
 import { computed } from 'vue'
 import { toComputedRefs } from '../utils'
 import { useClerkProvide } from './useClerkProvide'
 
 type UseSignInReturn =
-  | { isLoaded: false, signIn: undefined, setSession: undefined, setActive: undefined }
-  | { isLoaded: true, signIn: SignInResource, setSession: SetSession, setActive: SetActive }
+  | { isLoaded: false, signIn: undefined, setActive: undefined }
+  | { isLoaded: true, signIn: SignInResource, setActive: SetActive }
 
 export function useSignIn() {
   const { clerk, isClerkLoaded } = useClerkProvide()
 
   const result = computed<UseSignInReturn>(() => {
     if (!isClerkLoaded.value || !clerk.client)
-      return { isLoaded: false, signIn: undefined, setSession: undefined, setActive: undefined }
+      return { isLoaded: false, signIn: undefined, setActive: undefined }
 
     return {
       isLoaded: true,
       signIn: clerk.client.signIn,
-      setSession: clerk.setSession,
       setActive: clerk.setActive,
     }
   })
--- src/composables/useSignUp.ts
+++ src/composables/useSignUp.ts
@@ -1,23 +1,22 @@
-import type { SetActive, SetSession, SignUpResource } from '@clerk/types'
+import type { SetActive, SignUpResource } from '@clerk/types'
 import { computed } from 'vue'
 import { toComputedRefs } from '../utils'
 import { useClerkProvide } from './useClerkProvide'
 
 type UseSignUpReturn =
-  | { isLoaded: false, signUp: undefined, setSession: undefined, setActive: undefined }
-  | { isLoaded: true, signUp: SignUpResource, setSession: SetSession, setActive: SetActive }
+  | { isLoaded: false, signUp: undefined, setActive: undefined }
+  | { isLoaded: true, signUp: SignUpResource, setActive: SetActive }
 
 export function useSignUp() {
   const { clerk, isClerkLoaded } = useClerkProvide()
 
   const result = computed<UseSignUpReturn>(() => {
     if (!isClerkLoaded.value || !clerk.client)
-      return { isLoaded: false, signUp: undefined, setSession: undefined, setActive: undefined }
+      return { isLoaded: false, signUp: undefined, setActive: undefined }
 
     return {
       isLoaded: true,
       signUp: clerk.client.signUp,
-      setSession: clerk.setSession,
       setActive: clerk.setActive,
     }
   })
