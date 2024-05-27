94787164efcd5c5345658907151099a2d47ac538
wobsoriano <sorianorobertc@gmail.com>
2024-05-27 11:56:42 -0700

    fix file typo

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,27 +1,14 @@
 import type { Plugin } from 'vue'
-<<<<<<< HEAD
-import { provideClerkToApp } from './provideClerkToApp'
-<<<<<<< HEAD
 import type { IsomorphicClerkOptions } from './types'
 
-export { provideClerkToApp, provideClerkToVueApp }
-=======
-=======
-import { provideClerkToVueApp } from './provideClerkToApp'
->>>>>>> 61c6683 (chore(provideClerkToApp): rename to provideClerkToVueApp)
-
-import type { IsomorphicClerkOptions } from './types'
->>>>>>> 4fe9d2c (update playground)
+import { provideClerkToVueApp } from './provideClerkToVueApp'
 
 export const clerkPlugin: Plugin = {
   install(app, options: IsomorphicClerkOptions) {
     provideClerkToVueApp(app, options)
   },
 }
-<<<<<<< HEAD
-=======
 
 export {
   provideClerkToVueApp,
 }
->>>>>>> 4fe9d2c (update playground)
