c8f9b73c4e4632fb0d28f205bab5695279b07eea
wobsoriano <sorianorobertc@gmail.com>
2024-05-27 12:22:38 -0700

    tsup config update

--- global.d.ts
+++ global.d.ts
@@ -4,4 +4,5 @@ declare global {
   const PACKAGE_NAME: string
   const PACKAGE_VERSION: string
   const JS_PACKAGE_VERSION: string
+  const __DEV__: boolean
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -5,16 +5,21 @@ import { name, version } from './package.json'
 
 const clerkJsVersion = '5.5.1'
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  splitting: true,
-  clean: false,
-  dts: false,
-  esbuildPlugins: [vuePlugin()],
-  define: {
-    PACKAGE_NAME: `"${name}"`,
-    PACKAGE_VERSION: `"${version}"`,
-    JS_PACKAGE_VERSION: `"${clerkJsVersion}"`,
-  },
+export default defineConfig((overrideOptions) => {
+  const isWatch = !!overrideOptions.watch
+
+  return {
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    splitting: true,
+    clean: false,
+    dts: false,
+    esbuildPlugins: [vuePlugin()],
+    define: {
+      PACKAGE_NAME: `"${name}"`,
+      PACKAGE_VERSION: `"${version}"`,
+      JS_PACKAGE_VERSION: `"${clerkJsVersion}"`,
+      __DEV__: `${isWatch}`,
+    },
+  }
 })
