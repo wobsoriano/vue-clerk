114795208478685ca688d75300079ccdae301ba6
wobsoriano <sorianorobertc@gmail.com>
2024-06-21 22:02:43 -0700

    add toComputedRefs definition

--- src/utils/toComputedRefs.ts
+++ src/utils/toComputedRefs.ts
@@ -4,6 +4,10 @@ export type ToComputedRefs<T = any> = {
   [K in keyof T]: ComputedRef<T[K]>;
 }
 
+/**
+ * Converts a computed ref to an object of computed refs.
+ * This will allow the composables to be destructurable and still maintain reactivity.
+ */
 export function toComputedRefs<T extends object>(
   objectRef: ComputedRef<T>,
 ): ToComputedRefs<T> {
