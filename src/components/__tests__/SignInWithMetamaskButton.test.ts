9bcffafd234daa4d0509d232132c10f4e8a2f3e3
wobsoriano <sorianorobertc@gmail.com>
2024-07-27 16:56:14 -0700

    test: Add metamask sign in button test

---
+++ src/components/__tests__/SignInWithMetamaskButton.test.ts
@@ -0,0 +1,46 @@
+import { vi } from 'vitest'
+import { render, screen } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
+import { defineComponent, h } from 'vue'
+import SignInWithMetamaskButton from '../SignInWithMetamaskButton.vue'
+
+const mockAuthenticatewithMetamask = vi.fn()
+const originalError = console.error
+
+vi.mock('../../composables/useClerk', async () => {
+  return {
+    useClerk: () => ({
+      authenticateWithMetamask: mockAuthenticatewithMetamask,
+    }),
+  }
+})
+
+describe('<SignInWithMetamaskButton />', () => {
+  beforeAll(() => {
+    console.error = vi.fn()
+  })
+
+  beforeEach(() => {
+    mockAuthenticatewithMetamask.mockReset()
+  })
+
+  afterAll(() => {
+    console.error = originalError
+  })
+
+  it('calls clerk.authenticateWithMetamask when clicked', async () => {
+    render(SignInWithMetamaskButton)
+    const btn = screen.getByText('Sign in with Metamask')
+    await userEvent.click(btn)
+
+    expect(mockAuthenticatewithMetamask).toHaveBeenCalled()
+  })
+
+  it('uses text passed as children', async () => {
+    const Button = defineComponent(() => {
+      return () => h(SignInWithMetamaskButton, () => 'text')
+    })
+    render(Button)
+    screen.getByText('text')
+  })
+})
