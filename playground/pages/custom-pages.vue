39445c45636f7b5aef87a50921303aeed6266420
wobsoriano <sorianorobertc@gmail.com>
2024-08-24 20:30:21 -0700

    feat: Add custom pages and links to UserProfile component

--- playground/pages/custom-pages.vue
+++ playground/pages/custom-pages.vue
@@ -1,36 +1,20 @@
 <script setup lang="ts">
 import { UserProfile } from 'vue-clerk'
-import type { CustomPage } from '@clerk/types'
-
-const customPageIcon = ref<HTMLDivElement | null>(null)
-const customPageContent = ref<HTMLDivElement | null>(null)
-const customPages: CustomPage[] = [
-  {
-    url: 'custom-page',
-    label: 'Custom Page',
-    mountIcon: (el) => {
-      customPageIcon.value = el
-    },
-    unmountIcon: () => {
-      // do clean up if needed
-    },
-    mount: (el) => {
-      customPageContent.value = el
-    },
-    unmount: () => {
-      // do clean up if needed
-    },
-  },
-]
 </script>
 
 <template>
-  <UserProfile :custom-pages="customPages" />
-  <Teleport v-if="customPageIcon" :to="customPageIcon">
-    <div>👋</div>
-  </Teleport>
-  <Teleport v-if="customPageContent" :to="customPageContent">
-    <h1><b>Custom Page</b></h1>
-    <p>This is the content of the custom page.</p>
-  </Teleport>
+  <UserProfile>
+    <UserProfile.Page label="Custom page" url="custom">
+      <template #labelIcon>
+        <div>👋</div>
+      </template>
+      <p>Here is the user profile content</p>
+    </UserProfile.Page>
+    <UserProfile.Link label="Homepage" url="/">
+      <template #labelIcon>
+        <div>👋</div>
+      </template>
+    </UserProfile.Link>
+    <UserProfile.Page label="security" />
+  </UserProfile>
 </template>
