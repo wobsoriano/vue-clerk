c636ee309796987339eb6728ec99c5fb6f39f637
wobsoriano <sorianorobertc@gmail.com>
2024-08-31 02:19:32 -0700

    chore: Fix current user call

--- packages/nuxt/src/runtime/server/api/current-user.get.ts
+++ packages/nuxt/src/runtime/server/api/current-user.get.ts
@@ -4,5 +4,9 @@ import { clerkClient, getAuth } from 'h3-clerk'
 export default eventHandler((event) => {
   const { userId } = getAuth(event)
 
+  if (!userId) {
+    return null
+  }
+
   return clerkClient.users.getUser(userId)
 })
--- packages/nuxt/src/runtime/server/middleware.ts
+++ packages/nuxt/src/runtime/server/middleware.ts
@@ -1,9 +1,3 @@
 import { withClerkMiddleware } from 'h3-clerk'
-// @ts-expect-error: Nuxt imports
-import { useRuntimeConfig } from '#app'
 
-const publishableKey = useRuntimeConfig().public.clerk.publishableKey
-
-export default withClerkMiddleware({
-  publishableKey,
-})
+export default withClerkMiddleware()
