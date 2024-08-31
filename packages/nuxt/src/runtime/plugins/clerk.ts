a3a34456a1c35a56b19cc43c7380096c089877fa
wobsoriano <sorianorobertc@gmail.com>
2024-08-31 01:50:07 -0700

    chore: type fixes

--- packages/nuxt/src/runtime/plugins/clerk.ts
+++ packages/nuxt/src/runtime/plugins/clerk.ts
@@ -1,9 +1,7 @@
 import { clerkPlugin } from 'vue-clerk'
+import type { AuthObject } from '@clerk/backend'
 // @ts-expect-error: Nuxt imports
 import { defineNuxtPlugin, navigateTo, useRuntimeConfig, useState } from '#app'
-
-// TODO: Use AuthObject from @clerk/backend
-type AuthObject = Record<string, unknown>
 
 export default defineNuxtPlugin((nuxtApp) => {
   const serverInitialState = useState<AuthObject | undefined>('clerk-initial-state', () => undefined)
