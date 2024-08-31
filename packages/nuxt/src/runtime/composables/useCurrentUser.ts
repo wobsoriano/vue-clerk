65bcbf350776a0b00191a000256863aebfe27d4e
wobsoriano <sorianorobertc@gmail.com>
2024-08-31 13:25:49 -0700

    chore(nuxt): Manual watcher for unbuild

--- packages/nuxt/src/runtime/composables/useCurrentUser.ts
+++ packages/nuxt/src/runtime/composables/useCurrentUser.ts
@@ -2,7 +2,6 @@ import type { AuthObject } from 'h3-clerk'
 import type { AsyncData } from 'nuxt/app'
 import { useFetch } from '#imports'
 
-export function useCurrentUser(): Promise<AsyncData<AuthObject | null, Error>> {
-  // @ts-expect-error: Todo fix types
+export function useCurrentUser(): AsyncData<AuthObject | null, Error | null> {
   return useFetch('/api/_clerk/current-user')
 }
