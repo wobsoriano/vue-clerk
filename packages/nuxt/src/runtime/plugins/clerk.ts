a11639ee45f72cc2efd50805c402f88915595a8d
wobsoriano <sorianorobertc@gmail.com>
2024-08-31 17:49:24 -0700

    chore(nuxt): Update plugin order

--- packages/nuxt/src/runtime/plugins/clerk.ts
+++ packages/nuxt/src/runtime/plugins/clerk.ts
@@ -2,18 +2,22 @@ import { clerkPlugin } from 'vue-clerk'
 import type { AuthObject } from 'h3-clerk'
 import { defineNuxtPlugin, navigateTo, useRuntimeConfig, useState } from '#app'
 
-export default defineNuxtPlugin((nuxtApp: any) => {
-  const serverInitialState = useState<AuthObject | undefined>('clerk-initial-state', () => undefined)
+export default defineNuxtPlugin({
+  name: 'vue-clerk',
+  enforce: 'pre',
+  async setup(nuxtApp) {
+    const serverInitialState = useState<AuthObject | undefined>('clerk-initial-state', () => undefined)
 
-  if (process.env.server) {
-    const authContext = nuxtApp.ssrContext?.event.context.auth
-    serverInitialState.value = authContext ? JSON.parse(JSON.stringify(authContext)) : undefined
-  }
+    if (import.meta.server) {
+      const authContext = nuxtApp.ssrContext?.event.context.auth
+      serverInitialState.value = authContext ? JSON.parse(JSON.stringify(authContext)) : undefined
+    }
 
-  nuxtApp.vueApp.use(clerkPlugin, {
-    ...useRuntimeConfig().public.clerk,
-    routerPush: (to: string) => navigateTo(to),
-    routerReplace: (to: string) => navigateTo(to, { replace: true }),
-    initialState: serverInitialState.value,
-  })
+    nuxtApp.vueApp.use(clerkPlugin, {
+      ...useRuntimeConfig().public.clerk,
+      routerPush: (to: string) => navigateTo(to),
+      routerReplace: (to: string) => navigateTo(to, { replace: true }),
+      initialState: serverInitialState.value,
+    })
+  },
 })
