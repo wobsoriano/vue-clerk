c7321274581ff0696156cb0e01aedc8dbdbadf7b
wobsoriano <sorianorobertc@gmail.com>
2024-09-01 22:07:43 -0700

    chore: Update nuke script

--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -1,15 +1,24 @@
-rm -rf node_modules
-rm pnpm-lock.yaml
-rm -rf .turbo
+rm -rf node_modules pnpm-lock.yaml .turbo
 
-for d in packages/*
-do
-    echo $d
-    rm -r $d/node_modules $d/dist $d/.turbo
-done
+safe_remove() {
+    [ -d "$1" ] && rm -rf "$1"
+}
 
-for d in apps/*
-do
-    echo $d
-    rm -r $d/node_modules $d/dist $d/.turbo
+dirs_to_remove=(
+    "node_modules"
+    "dist"
+    ".nuxt"
+    ".vitepress/cache"
+    ".vitepress/dist"
+    ".output"
+    ".turbo"
+)
+
+for base in packages apps; do
+    for d in "$base"/*; do
+        echo "Cleaning $d"
+        for dir in "${dirs_to_remove[@]}"; do
+            safe_remove "$d/$dir"
+        done
+    done
 done
