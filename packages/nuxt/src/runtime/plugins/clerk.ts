5290d0acb871caed8e8cdcf91ff56097980d362e
wobsoriano <sorianorobertc@gmail.com>
2024-09-18 12:09:32 -0700

    chore: simplify initial state vars

--- packages/nuxt/src/runtime/plugins/clerk.ts
+++ packages/nuxt/src/runtime/plugins/clerk.ts
@@ -5,18 +5,17 @@ import { defineNuxtPlugin, navigateTo, useRuntimeConfig, useState } from '#impor
 export default defineNuxtPlugin({
   name: 'vue-clerk',
   setup(nuxtApp) {
-    const serverInitialState = useState<InitialState | undefined>('clerk-initial-state', () => undefined)
+    const initialState = useState<InitialState | undefined>('clerk-initial-state', () => undefined)
 
     if (import.meta.server) {
-      const initialState = nuxtApp.ssrContext?.event.context.__clerk_initial_state
-      serverInitialState.value = initialState
+      initialState.value = nuxtApp.ssrContext?.event.context.__clerk_initial_state
     }
 
     nuxtApp.vueApp.use(clerkPlugin, {
       ...useRuntimeConfig().public.clerk,
       routerPush: (to: string) => navigateTo(to),
       routerReplace: (to: string) => navigateTo(to, { replace: true }),
-      initialState: serverInitialState.value,
+      initialState: initialState.value,
     })
   },
 })
