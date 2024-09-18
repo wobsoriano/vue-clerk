45ea3968bd9ae7114dad10631da1a29dcda32b5c
wobsoriano <sorianorobertc@gmail.com>
2024-09-18 15:09:42 -0700

    chore(nuxt): allow condition for roles and permissions

--- packages/nuxt/src/runtime/middleware/auth.ts
+++ packages/nuxt/src/runtime/middleware/auth.ts
@@ -4,9 +4,13 @@ import { defineNuxtRouteMiddleware, navigateTo } from '#imports'
 
 export default defineNuxtRouteMiddleware((to) => {
   const { userId, has } = useAuth()
-  const { permission, role, guestRedirectUrl = '/' } = to.meta?.auth || {}
+  const { condition, permission, role, guestRedirectUrl = '/' } = to.meta?.auth || {}
 
   if ((permission || role) && !has.value?.({ permission, role } as Parameters<CheckAuthorizationWithCustomPermissions>[0])) {
+    return navigateTo(guestRedirectUrl)
+  }
+
+  if (typeof condition === 'function' && !condition(has.value!)) {
     return navigateTo(guestRedirectUrl)
   }
 
