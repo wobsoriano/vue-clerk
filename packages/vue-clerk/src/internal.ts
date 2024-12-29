0d284116cc169ade9953d761f555c89f7eed9bfc
wobsoriano <sorianorobertc@gmail.com>
2024-12-29 12:29:31 -0800

    chore: clean up deprecation message

--- packages/vue-clerk/src/internal.ts
+++ packages/vue-clerk/src/internal.ts
@@ -1,4 +1,4 @@
 export { setErrorThrowerOptions } from './errors/errorThrower'
 
 export { setClerkJsLoadingErrorPackageName } from '@clerk/shared/loadClerkJsScript'
-export { logDeprecation } from './utils'
+export { getDeprecationMessage } from './utils'
--- packages/vue-clerk/src/utils/deprecate.ts
+++ packages/vue-clerk/src/utils/deprecate.ts
@@ -1,14 +1,14 @@
-export function logDeprecation() {
-  console.log(`
+export function getDeprecationMessage() {
+  return `
 DEPRECATION NOTICE
 ==================
 
-This project has graduated to an official SDK!
+The vue-clerk package has graduated to an official SDK!
 
 Please visit our migration guide:
 https://clerk.com/docs/references/vue/migrating-from-vue-community-sdk
 
 The community SDK will continue to work but will no longer
 receive updates or security patches.
-`.trim())
+`
 }
