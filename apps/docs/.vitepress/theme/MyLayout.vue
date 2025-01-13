8e72ebd63408687a91d78ce0dbbbb4c7d30836a7
wobsoriano <sorianorobertc@gmail.com>
2025-01-12 21:27:52 -0800

    chore: Fix deprecation message

---
+++ apps/docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import Banner from '../components/Banner.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #layout-top>
+      <Banner />
+    </template>
+  </Layout>
+</template>
--- apps/docs/.vitepress/theme/index.js
+++ apps/docs/.vitepress/theme/index.js
@@ -1,5 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
+import MyLayout from './MyLayout.vue'
 
 export default {
   extends: DefaultTheme,
+  // override the Layout with a wrapper component that
+  // injects the slots
+  Layout: MyLayout,
 }
